cfdc4fc729806f90bdf17d23f84d00d0109e097a
Rob Bell <rob.bell@asos.com>
2016-09-08 17:47:00 +0100

    Add basic consumber

---
+++ RabbitMqSamples.Consumer/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace RabbitMqSamples.Consumer
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory { HostName = "localhost" };
+
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "hello",
+                    durable: false,
+                    exclusive: false,
+                    autoDelete: false,
+                    arguments: null);
+
+                var consumer = new EventingBasicConsumer(channel);
+
+                consumer.Received += (model, ea) =>
+                {
+                    var message = Encoding.UTF8.GetString(ea.Body);
+                    Console.WriteLine("[x] Received {0}", message);
+                };
+
+                channel.BasicConsume(queue: "hello", noAck: true, consumer: consumer);
+
+                Console.WriteLine("Press [enter] to exit");
+                Console.ReadLine();
+            }
+        }
+    }
+}
